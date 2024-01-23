2a605a9bf45637a42c9cee43efe0cc25f6f58bdc
ritesh-blog27 <riteshminecraft27@gmail.com>
2024-01-23 21:53:10 +0530

    cors issue to solve 1

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pickle
 from symptomsConverter import SymptomsConverter
-from flask import Flask, render_template, request, url_for
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
 import os
+
 app = Flask(__name__)
+CORS(app)
 
 PORT = 3000
 PORT = os.environ['PORT'] or 3000
@@ -12,10 +15,12 @@ sym_model = pickle.load(open('Symptom_Model1.sav', 'rb'))
 heart_model = pickle.load(open('Health_Model1.sav', 'rb'))
 
 @app.route('/')
+@cross_origin(origin='*')
 def home():
     return "Hello World!"
 
 @app.route('/disease', methods=['POST'])
+@cross_origin(origin='*')
 def disease():
     inputs = request.json['inputs']
     inputs = SymptomsConverter().converts(inputs)
@@ -25,6 +30,7 @@ def disease():
     }
 
 @app.route('/heart', methods=["POST"])
+@cross_origin(origin='*')
 def heart():
     inputs = request.json['inputs']
     # inputs = [float(i) for i in inputs]
